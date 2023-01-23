15b98e029baeb4f361cf80cd20d0484b8cff0689
JamesChaves <james.chaves@sinco.com.co>
2023-01-23 02:29:51 -0500

    Setting up a checkbox system of validate

---
+++ BeyondTheDevTest/filterResults.js
@@ -0,0 +1,208 @@
+let productData = {
+        "product_name": "        LE 100W Equivalent LED Light Bulbs, 14W 1600 Lumens Daylight White 5000K Non-Dimmable, A19 E26 Standard Base, 11000 Hour Lifetime, Pack of 6       ",
+        "color": "Daylight White",
+        "status": "    Currently unavailable.   ",
+        "product_image": "https://m.media-amazon.com/images/I/31Fuy31sa7L._AC_SR38,50_.jpg",
+        "about_items": [
+            "11000 Hours Lifespan - Each bulb is expected to last over 10 years (based on 3 hours/day), saving you the cost and hassle of frequent bulb replacement.",
+            "Energy Efficient - Replaces 100W incandescent bulb with 14W LED and saves you 86% in energy use, making it an economical alternative to incandescent bulbs.",
+            "Standard E26 Base - Easily screw into any light fixture with medium screw base. Widely used in table lamps, floor lamps and open pendant fixtures for bedroom, living room, dining room or anywhere for home or commercial use.",
+            "Energizing Daylight - With a color temperature of 5000 kelvin, these LED bulbs provide a calm and comfortable white light for any room.",
+            "What You Get - 6 Non-dimmable A19 E26 LED bulbs. (Not eligible for shipments to California.)"
+        ],
+        "additional_information": {
+            "ASIN": "B086C37632",
+            "Customer Reviews": " 4.7 out of 5 stars     5,071 ratings 4.7 out of 5 stars",
+            "Best Sellers Rank": "#2,590 in Industrial & Scientific (See Top 100 in Industrial & Scientific) #219 in LED Bulbs #3,435 in Household Supplies ",
+            "Date First Available": "March 25, 2020",
+            "Brand": "LE",
+            "Light Type": "LED",
+            "Special Feature": " ✔ Not Dimmable ✔ 14W=100W ✔ Daylight White 5000K ✔ Instant On ✔ Shatter resistant ✔ Shock and vibration resistant ✔ Non-bug-attracting ✔ No radiation and no flicker ✔ Not Dimmable ✔ 14W=100W ✔ Daylight White 5000K See more",
+            "Wattage": "14 watts",
+            "Bulb Shape Size": "A19",
+            "Bulb Base": "‎E26",
+            "Incandescent Equivalent Wattage": "‎100 Watts",
+            "Specific Uses": "‎General purpose",
+            "Light Color": "‎5000K daylight white",
+            "Voltage": "‎120 Volts (AC)",
+            "Unit Count": "‎6 Count",
+            "Color Temperature": "‎5000 Kelvin",
+            "Item Package Quantity": "‎1",
+            "Luminous Flux": "‎1600 Lumen",
+            "Shape": "‎A19",
+            "Size": "‎6 Count (Pack of 1)",
+            "Connectivity Technology": "‎Normal bulb",
+            "Indoor/Outdoor Usage": "‎Indoor",
+            "Controller Type": "‎Push Button",
+            "Style": "‎Modern",
+            "Included Components": "‎Bulb included",
+            "Power Source": "‎AC",
+            "Color Rendering Index": "‎80.00",
+            "Manufacturer": "‎LE",
+            "Part Number": "‎100080-DW-US-6",
+            "Item Weight": "‎10.9 ounces",
+            "Product Dimensions": "‎2.36 x 2.36 x 4.61 inches",
+            "Item model number": "‎100080-DW-US-6",
+            "Is Discontinued By Manufacturer": "‎No",
+            "Assembled Height": "‎4.61 inches",
+            "Assembled Length": "‎2.36 inches",
+            "Assembled Width": "‎2.36 inches",
+            "Color": "‎Daylight White",
+            "Special Features": "‎✔ Not Dimmable<br>✔ 14W=100W<br>✔ Daylight White 5000K<br>✔ Instant On <br>✔ Shatter resistant<br>✔ Shock and vibration resistant<br>✔ Non-bug-attracting<br>✔ No radiation and no flicker",
+            "Shade Material": "‎Plastic",
+            "Light Direction": "‎Up/Down Light",
+            "Batteries Included?": "‎No",
+            "Batteries Required?": "‎No",
+            "Type of Bulb": "‎LED",
+            "Bulb Features": "‎Not Dimmable",
+            "Average Life": "‎11000 Hours",
+            "Bulb Diameter": "‎60 Millimeters",
+        },
+        "technical_details": {
+            "ASIN": "B086C37632",
+            "Customer Reviews": " 4.7 out of 5 stars     5,071 ratings 4.7 out of 5 stars",
+            "Best Sellers Rank": "#2,590 in Industrial & Scientific (See Top 100 in Industrial & Scientific) #219 in LED Bulbs #3,435 in Household Supplies ",
+            "Date First Available": "March 25, 2020",
+            "Brand": "LE",
+            "Light Type": "LED",
+            "Special Feature": " ✔ Not Dimmable ✔ 14W=100W ✔ Daylight White 5000K ✔ Instant On ✔ Shatter resistant ✔ Shock and vibration resistant ✔ Non-bug-attracting ✔ No radiation and no flicker ✔ Not Dimmable ✔ 14W=100W ✔ Daylight White 5000K See more",
+            "Wattage": "14 watts",
+            "Bulb Shape Size": "A19",
+            "Bulb Base": "‎E26",
+            "Incandescent Equivalent Wattage": "‎100 Watts",
+            "Specific Uses": "‎General purpose",
+            "Light Color": "‎5000K daylight white",
+            "Voltage": "‎120 Volts (AC)",
+            "Unit Count": "‎6 Count",
+            "Color Temperature": "‎5000 Kelvin",
+            "Item Package Quantity": "‎1",
+            "Luminous Flux": "‎1600 Lumen",
+            "Shape": "‎A19",
+            "Size": "‎6 Count (Pack of 1)",
+            "Connectivity Technology": "‎Normal bulb",
+            "Indoor/Outdoor Usage": "‎Indoor",
+            "Controller Type": "‎Push Button",
+            "Style": "‎Modern",
+            "Included Components": "‎Bulb included",
+            "Power Source": "‎AC",
+            "Color Rendering Index": "‎80.00",
+            "Manufacturer": "‎LE",
+            "Part Number": "‎100080-DW-US-6",
+            "Item Weight": "‎10.9 ounces",
+            "Product Dimensions": "‎2.36 x 2.36 x 4.61 inches",
+            "Item model number": "‎100080-DW-US-6",
+            "Is Discontinued By Manufacturer": "‎No",
+            "Assembled Height": "‎4.61 inches",
+            "Assembled Length": "‎2.36 inches",
+            "Assembled Width": "‎2.36 inches",
+            "Color": "‎Daylight White",
+            "Special Features": "‎✔ Not Dimmable<br>✔ 14W=100W<br>✔ Daylight White 5000K<br>✔ Instant On <br>✔ Shatter resistant<br>✔ Shock and vibration resistant<br>✔ Non-bug-attracting<br>✔ No radiation and no flicker",
+            "Shade Material": "‎Plastic",
+            "Light Direction": "‎Up/Down Light",
+            "Batteries Included?": "‎No",
+            "Batteries Required?": "‎No",
+            "Type of Bulb": "‎LED",
+            "Bulb Features": "‎Not Dimmable",
+            "Average Life": "‎11000 Hours",
+            "Bulb Diameter": "‎60 Millimeters",
+        },
+        "product_details": {
+            "ASIN": "B086C37632",
+            "Customer Reviews": " 4.7 out of 5 stars     5,071 ratings 4.7 out of 5 stars",
+            "Best Sellers Rank": "#2,590 in Industrial & Scientific (See Top 100 in Industrial & Scientific) #219 in LED Bulbs #3,435 in Household Supplies ",
+            "Date First Available": "March 25, 2020",
+            "Brand": "LE",
+            "Light Type": "LED",
+            "Special Feature": " ✔ Not Dimmable ✔ 14W=100W ✔ Daylight White 5000K ✔ Instant On ✔ Shatter resistant ✔ Shock and vibration resistant ✔ Non-bug-attracting ✔ No radiation and no flicker ✔ Not Dimmable ✔ 14W=100W ✔ Daylight White 5000K See more",
+            "Wattage": "14 watts",
+            "Bulb Shape Size": "A19",
+            "Bulb Base": "‎E26",
+            "Incandescent Equivalent Wattage": "‎100 Watts",
+            "Specific Uses": "‎General purpose",
+            "Light Color": "‎5000K daylight white",
+            "Voltage": "‎120 Volts (AC)",
+            "Unit Count": "‎6 Count",
+            "Color Temperature": "‎5000 Kelvin",
+            "Item Package Quantity": "‎1",
+            "Luminous Flux": "‎1600 Lumen",
+            "Shape": "‎A19",
+            "Size": "‎6 Count (Pack of 1)",
+            "Connectivity Technology": "‎Normal bulb",
+            "Indoor/Outdoor Usage": "‎Indoor",
+            "Controller Type": "‎Push Button",
+            "Style": "‎Modern",
+            "Included Components": "‎Bulb included",
+            "Power Source": "‎AC",
+            "Color Rendering Index": "‎80.00",
+            "Manufacturer": "‎LE",
+            "Part Number": "‎100080-DW-US-6",
+            "Item Weight": "‎10.9 ounces",
+            "Product Dimensions": "‎2.36 x 2.36 x 4.61 inches",
+            "Item model number": "‎100080-DW-US-6",
+            "Is Discontinued By Manufacturer": "‎No",
+            "Assembled Height": "‎4.61 inches",
+            "Assembled Length": "‎2.36 inches",
+            "Assembled Width": "‎2.36 inches",
+            "Color": "‎Daylight White",
+            "Special Features": "‎✔ Not Dimmable<br>✔ 14W=100W<br>✔ Daylight White 5000K<br>✔ Instant On <br>✔ Shatter resistant<br>✔ Shock and vibration resistant<br>✔ Non-bug-attracting<br>✔ No radiation and no flicker",
+            "Shade Material": "‎Plastic",
+            "Light Direction": "‎Up/Down Light",
+            "Batteries Included?": "‎No",
+            "Batteries Required?": "‎No",
+            "Type of Bulb": "‎LED",
+            "Bulb Features": "‎Not Dimmable",
+            "Average Life": "‎11000 Hours",
+            "Bulb Diameter": "‎60 Millimeters"
+        }
+    },
+
+jsonToCheckBoxes = (productArray,name) => {
+        (typeof productArray === 'string') ? $('#checkList').append(`<input name="${name}"type="checkbox" checked="true" value="${productArray}" /> ${name} : ${productArray}`)
+        : Object.entries(productArray).forEach(function (obj) {
+            (typeof obj[1] !== 'object') ?  $('#checkList').append(`<input name="${name}"type="checkbox" checked="true" value="${obj[0]}" /> ${obj[0]} : ${obj[1]}`)
+            : (!Array.isArray(obj[1])) ? $('#checkList').append(`<input name="${name}"type="checkbox" checked="true" value="${obj[0]}"/> ${obj[0]} ${jsonToCheckBoxes(obj[1])}`)  : 
+             $('#checkList').append(`<input name="${name}"type="checkbox" checked="true" value="${obj.split('-').newObj[0]}" /> ${obj.split('-').newObj[0]} : ${obj.split('-').newObj[1]}}`);
+        });
+    };
+
+$.each(Object.keys(productData), ( i, val ) => {
+    jsonToCheckBoxes(productData[`${val}`],val)
+    
+        // $.each(Object.keys(productData), ( i, val ) => {
+        //     jsonToCheckBoxes(productData[`${val}`],val)
+        //   });
+});
+       
+let updateValidatedJson = ()=>{
+   $.each([...$("#checkList input")], (i, val ) => {
+    console.log(val)
+    if(!val.checked) {
+        $.each(Object.keys(productData), ( i, val ) => {
+            // productData[`${val}`] ? delete productData[`${val}`] :
+            console.log(Object.keys(productData[`${val}`]))
+            // $.each(Object.keys(productData[`${val}`]),( i, val ) => {
+            // //     console.log(Object.keys(productData[`${val}`]),i,val)
+            // })
+          });
+    } else{
+       
+    } 
+        })
+    }
+let downloadValidatedJson = ()=>{
+    $('head').append('<a download="productData.json"></a>')
+    $('a[download="productData.json"]').attr('href', window.URL.createObjectURL(new Blob([JSON.stringify(productData,null,3)],{type:'text'})))
+    $('a[download="productData.json"]')[0].click()
+     }
+
+console.log(Object.keys(productData))
+
+
+// jsonToCheckBoxes = (productArray,) => {
+//    Object.entries(productArray).forEach(function (obj) {
+//        (typeof obj[1] !== 'object') ?  $('#checkList').append(`<input type="checkbox" checked="true" value="${obj[0]}" /> ${obj[0]} : ${obj[1]}`)
+//        : (!Array.isArray(obj[1])) ? $('#checkList').append(`<input type="checkbox" checked="true" value="${obj[0]}"/> ${obj[0]} ${jsonToCheckBoxes(obj[1])}`)  : console.log('array')
+//    });
+//    $('#checkList').append('<button> Download JSON </button>')
+// };
+
---
+++ officialScrapper.js
@@ -0,0 +1,15 @@
+tableToJsonObj = (table) => {
+    let jsonObj = {};
+    $.each([...table], (i,val) => jsonObj[`${val.innerText.split('\t')[0]}`] = `${val.innerText.split('\t')[1].split('\n').join(' ')}`);
+    return jsonObj
+},
+productData = {
+    product_name: $('#productTitle').text(),
+    color : $('.a-row .selection').text(),
+    status : $('#availability span').text(),
+    product_image : [...$('span#a-autoid-9-announce img')][0].src,
+    about_items: jQuery.map( [...$('div#feature-bullets ul li')],  bullet => bullet = bullet.innerText),
+    additional_information : tableToJsonObj($('table#productDetails_detailBullets_sections1 tr')),
+    technical_details : tableToJsonObj($('table#productDetails_techSpec_section_1 tr')),
+    product_details: tableToJsonObj($('table.a-normal tr')) ,
+}
